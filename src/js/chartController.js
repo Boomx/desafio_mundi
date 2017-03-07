2f29d6f51d814a5177d576c033e0c88630e66d91
Denilson Junior <jundenilson@gmail.com>
2017-03-07 20:25:27 -0300

    :rotating_light: Chart ratio and colors adjustments

--- src/js/chartController.js
+++ src/js/chartController.js
@@ -1,30 +1,29 @@
 export function beginChart(repository) {
+    Chart.defaults.global.maintainAspectRatio = false;
     var ctx = document.getElementById("myChart");
     var data = {
-        // labels: ["January", "February", "March", "April", "May", "June", "July"],
         labels: repository.commitsAnalysis.intervals,
         datasets: [{
                 label: "Commits",
                 fill: true,
                 lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
+                backgroundColor: "rgba(0, 120, 193,0.4)",
+                borderColor: "rgba(0, 120, 193,1)",
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
+                pointBorderColor: "rgba(0, 120, 193,1)",
                 pointBackgroundColor: "#fff",
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBackgroundColor: "rgba(0, 120, 193,1)",
                 pointHoverBorderColor: "rgba(220,220,220,1)",
                 pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                // data: [65, 59, 80, 81, 56, 55, 40],
+                pointRadius: 3,
+                pointHitRadius: 20,
                 data: repository.commitsAnalysis.commitsPerInterval,
-                spanGaps: false,
+                spanGaps: true,
             }
         ],
 
